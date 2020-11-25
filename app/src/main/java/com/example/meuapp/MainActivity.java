1c00a20f9e47a4bb98d6e32a0f7da657f7c69417
Vinicius <viniciusfsa@gmal.com>
2020-11-25 16:25:55 -0300

    Commit inicial

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.meuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.btnPrincipal);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SegundaActivity.class));
+            }
+        });
 
+    }
 
+    public void abrirSegundaActivity(View view){
+        startActivity(new Intent(getBaseContext(),SegundaActivity.class));
     }
 }
--- app/src/main/java/com/example/meuapp/SegundaActivity.java
+++ app/src/main/java/com/example/meuapp/SegundaActivity.java
@@ -1,4 +1,26 @@
 package com.example.meuapp;
 
-public class SegundaActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SegundaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("SegundaActivity","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("SegundaActivity","onPause");
+    }
 }
